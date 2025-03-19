90e0940b95619b26bb6117d9a11e1c769d88430f
paule32 <paule32.jk@gmail.com>
2025-03-19 10:29:35 +0100

    update

---
+++ out/chm/alias.h
@@ -0,0 +1,4 @@
+Willkommen=Willkommen.htm
+NeuesThema=NeuesThema.htm
+Fehler-Nachrichten=Fehler-Nachrichten.htm
+Vorlage=Vorlage.htm
---
+++ out/chm/contextnb.h
@@ -0,0 +1,4 @@
+#define Willkommen 0
+#define NeuesThema 29
+#define Fehler-Nachrichten 1
+#define Vorlage 4
